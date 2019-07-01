b0a3286df709806dfd7ae6ce4c77b2adde0601df
zhangjian <zhangjian1@keking.cn>
2019-07-01 10:20:40 +0800

    仿照谷歌percent库写的一个RelativeLayout百分比布局，

---
+++ app/src/main/java/test/com/testapp/PercentActivity.java
@@ -0,0 +1,16 @@
+package test.com.testapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by zhangjian on 2019/6/25 13:44
+ */
+public class PercentActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_percent);
+    }
+}
