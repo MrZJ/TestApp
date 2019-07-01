262eb185b65eae76165f7e74fa570ebf057634ec
zhangjian <zhangjian1@keking.cn>
2019-07-01 10:17:07 +0800

    pathmeasure练习，做一个小车动画

---
+++ app/src/main/java/test/com/testapp/PathMeasureActivity.java
@@ -0,0 +1,16 @@
+package test.com.testapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+
+/**
+ * Created by zhangjian on 2019/4/9 15:00
+ */
+public class PathMeasureActivity extends FragmentActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pathmeasure);
+    }
+}
